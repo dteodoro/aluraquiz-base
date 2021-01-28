3470b2f8e9d3ee8d087527777d922f2e3510301c
dteodoro <dario.teodoro@live.com>
2021-01-28 20:18:18 -0300

    codigo ao final da primeira aula

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,41 @@
 import styled from 'styled-components'
 
+import BackgroundApp from '../src/components/BackgroundApp';
+import QuizContainer from '../src/components/QuizContainer';
+import Widget from '../src/components/Widget';
+import db from '../db.json';
+import GitHubCorner from '../src/components/GitHubCorner';
+import Footer from '../src/components/Footer';
+import QuizLogo from '../src/components/QuizLogo';
+
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <BackgroundApp bgImage ={db.bg}> 
+      <QuizContainer>
+        <QuizLogo/>
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+        
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+            <p>lorem ipsum sit amet... </p>
+          </Widget.Content>
+        </Widget>
+        <Footer/>
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/dteodoro" />
+    </BackgroundApp>
+  );
 }
